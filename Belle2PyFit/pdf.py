4ab89edb9766b6ba91febe96c605b208230c40d6
Thomas Keck <thomas.keck2@kit.edu>
2016-11-04 19:43:18 +0100

    Rough infrastructure works now, and is fast, still needs refactoring and the fitting fails at the moment

--- Belle2PyFit/pdf.py
+++ Belle2PyFit/pdf.py
@@ -1,102 +1,157 @@
 #!/usr/bin/env python3
 
+import copy
 import numpy as np
 import scipy
 import scipy.optimize
 import scipy.stats
 import matplotlib.pyplot as plt
-import functools
+
+
+class ScipyDistributionGenerator(scipy.stats.rv_continuous):
+    def __init__(self, fit_pdf):
+        self.fit_pdf = fit_pdf
+        shapes = ', '.join(self.fit_pdf.parameters.keys())
+        super(ScipyDistributionGenerator, self).__init__(shapes=shapes)
+
+    def _pdf(self, x, *parameters):
+        parameters = {key: parameter for key, parameter in zip(self.fit_pdf.parameters.keys(), parameters)}
+        return np.sum(parameters['{}_norm'.format(label)] *
+                      distribution.pdf(x, **{key[len(label)+1:]: parameters[key] for key in parameters.keys() if key.startswith(label) and key != '{}_norm'.format(label)})
+                      for label, distribution in zip(self.fit_pdf.labels, self.fit_pdf.distributions))
+    
+    def _cdf(self, x, *parameters):
+        parameters = {key: parameter for key, parameter in zip(self.fit_pdf.parameters.keys(), parameters)}
+        return np.sum(parameters['{}_norm'.format(label)] *
+                      distribution.cdf(x, **{key[len(label)+1:]: parameters[key] for key in parameters.keys() if key.startswith(label) and key != '{}_norm'.format(label)})
+                      for label, distribution in zip(self.fit_pdf.labels, self.fit_pdf.distributions))
+    
+    def _rvs(self, *parameters):
+        parameters = {key: parameter for key, parameter in zip(self.fit_pdf.parameters.keys(), parameters)}
+        probabilities = np.array([parameters['{}_norm'.format(label)] for label in self.fit_pdf.labels])
+        probabilities /= probabilities.sum()
+        choices = np.random.choice(len(self.fit_pdf.distributions), size=self._size, p=probabilities)
+        result = np.zeros(self._size)
+        for i, (label, distribution) in enumerate(zip(self.fit_pdf.labels, self.fit_pdf.distributions)):
+            mask = choices == i
+            component_parameters = {key[len(label)+1:]: parameters[key] for key in parameters.keys() if key.startswith(label) and key != '{}_norm'.format(label)}
+            print(distribution, component_parameters)
+            result[mask] = distribution.rvs(size=mask.sum(), **component_parameters)
+        return result
+
+    def _argcheck(self, *args):
+        return 1
+
+
+class ScipyTemplateDistribution(scipy.stats.rv_continuous):
+    def __init__(self, data, **kwargs):
+        pdf, bins = np.histogram(data, density=True, **kwargs)
+        cdf = np.cumsum(pdf * (bins - np.roll(bins, 1))[1:])
+        self.template_bins = bins
+        self.template_pdf = np.hstack([0.0, pdf, 0.0])
+        self.template_cdf = np.hstack([0.0, cdf, 1.0])
+        self.template_bin_centers = (bins - (bins - np.roll(bins, 1)) / 2.0) [1:]
+        super(ScipyTemplateDistribution, self).__init__()
+
+    def _pdf(self, x):
+        return self.template_pdf[np.digitize(x, bins=self.template_bins)]
+    
+    def _cdf(self, x):
+        return self.template_cdf[np.digitize(x, bins=self.template_bins)]
+    
+    def _rvs(self):
+        probabilities = self.template_pdf[1:-1]
+        probabilities /= probabilities.sum()
+        choices = np.random.choice(len(self.template_pdf) - 2, size=self._size, p=probabilities)
+        return self.template_bin_centers[choices]
+
 
 class PDF(object):
-    def __init__(self, func, free_parameters={}, fixed_parameters={}, label=''):
-        self.func = func
-        self.free_parameters = free_parameters
-        self.fixed_parameters = fixed_parameters
-        self.label = label
+    @classmethod
+    def from_scipy(cls, label, scipy_distribution, norm = 1.0, **parameters):
+        parameters = {'{}_{}'.format(label, key): value for key, value in parameters.items()}
+        parameters['{}_norm'.format(label)] = norm
+        return cls([label], [scipy_distribution], parameters)
+
+    @classmethod
+    def from_data(cls, label, data, norm = 1.0, **kwargs):
+        return cls([label], [ScipyTemplateDistribution(data, **kwargs)], {'{}_norm'.format(label): norm})
+
+    def __init__(self, labels, distributions, parameters, constraints={}, sub_pdfs=[]):
+        self.labels = labels
+        self.distributions = distributions
+        self.parameters = parameters
+        self.scipy = ScipyDistributionGenerator(self)
+        self.constraints = constraints
+        self.sub_pdfs = sub_pdfs if sub_pdfs else [self]
 
     def __add__(self, other):
-        def func(x, **free_parameters):
-            return (self.func(x, **{p: free_parameters['{}_{}'.format(self.label, p)] for p in self.free_parameters}) +
-                    other.func(x, **{p: free_parameters['{}_{}'.format(other.label, p)] for p in other.free_parameters}))
+        parameters = {}
+        parameters.update(self.parameters)
+        parameters.update(other.parameters)
+        constraints = {}
+        constraints.update(self.constraints)
+        constraints.update(other.constraints)
+        return PDF(self.labels + other.labels, self.distributions + other.distributions, parameters, constraints, self.sub_pdfs + other.sub_pdfs)
 
-        free_parameters = {}
-        for k, v in self.free_parameters.items():
-            free_parameters['{}_{}'.format(self.label, k)] = v
-        for k, v in other.free_parameters.items():
-            free_parameters['{}_{}'.format(other.label, k)] = v
-        
-        fixed_parameters = {}
-        for k, v in self.fixed_parameters.items():
-            fixed_parameters['{}_{}'.format(self.label, k)] = v
-        for k, v in other.fixed_parameters.items():
-            fixed_parameters['{}_{}'.format(other.label, k)] = v
+    def __iter__(self):
+        return iter(self.sub_pdfs)
 
-        return PDF(func, free_parameters, fixed_parameters, label='({}+{})'.format(self.label, other.label))
+    def add_constraint(self, parameter, function):
+        self.constraints[parameter] = function
 
-    def __call__(self, data, **free_parameters):
-        return self.func(data, **free_parameters, **self.fixed_parameters)
+    def fix_parameter(self, parameter):
+        self.add_constraint(parameter, lambda p: self.parameters[parameter])
 
-    def GetInitialParameters(self):
-        return np.array(list(self.free_parameters.values()))
+    def get_free_parameters(self):
+        return {parameter: value for parameter, value in self.parameters.items() if parameter not in self.constraints}
 
-    def GetFreeParametersFromFitResult(self, res):
-        return {k: v for k, v in zip(self.free_parameters.keys(), res.x)}
+    def get_initial_parameters(self):
+        return np.array(list(self.get_free_parameters().values()))
 
-    def GetLoss(self, method):
-        if method == 'MaximumLikelihood':
-            def loss(parameters, data):
-                return -np.sum(np.log(self.func(data, **{k: parameters[i] for i, k in enumerate(self.free_parameters)})))
-        else:
-            return NotImplemented
+    def get_result_parameters(self, res):
+        parameters = {k: v for k, v in zip(self.get_free_parameters().keys(), res.x)}
+        for parameter, function in self.constraints.items():
+            parameters[parameter] = function(parameters)
+        return parameters 
+
+    def get_maximumLikelihood_loss(self):
+        def loss(free_parameters, data):
+            parameters = {key: free_parameters[i] for i, key in enumerate(self.get_free_parameters().keys())}
+            for parameter, function in self.constraints.items():
+                parameters[parameter] = function(parameters)
+            return -np.sum(np.log(self.scipy.pdf(data, **parameters)))
         return loss
-
-    def GetScipyDistribution(self, parameters):
-        class ScipyDistributionGenerator(scipy.stats.rv_continuous):
-            cdf_cache = {}
-            def _pdf(other_self, data):
-                return self.func(data, **self.free_parameters, **self.fixed_parameters)
-            def _cdf(other_self, data):
-                data = (data * 100).astype(int)
-                result = []
-                for d in data:
-                    if d not in other_self.cdf_cache:
-                        other_self.cdf_cache[d] = super(ScipyDistributionGenerator, other_self)._cdf(d / 100.0)
-                    result.append(other_self.cdf_cache[d])
-                return np.array(result)
-                
-            # TODO Implement _cdf for faster rvs generation
-        return ScipyDistributionGenerator()
-
-
-class Template(object):
-    def __init__(self, histogram):
-        self.bins, self.counts = histogram
-        total_counts = np.sum(self.counts)
-        self.pdf = self.counts / total_counts
-
-    def __call__(self, data):
-        return self.pdf[np.digitize(data, self.bins)]
 
 
 if __name__ == '__main__':
 
-    Signal =  PDF(scipy.stats.norm.pdf, free_parameters={'loc': 0.5}, fixed_parameters={'scale': 0.1}, label='Signal')
-    Background =  PDF(scipy.stats.norm.pdf, free_parameters={'loc': 0.5}, fixed_parameters={'scale': 1.0}, label='Background')
-    TemplateBackground =  PDF(Template(histogram), free_parameters={'loc': 0.5}, fixed_parameters={'scale': 1.0}, label='Background')
-    Model = Signal + Background
+    signal = PDF.from_scipy('signal', scipy.stats.norm, loc=0.5, scale=0.1)
+    
+    background = PDF.from_scipy('background', scipy.stats.norm, loc=0.5, scale=1.0)
+    
+    continuum_data = scipy.stats.norm.rvs(size=10000, loc=0, scale=2.0)
+    continuum = PDF.from_data('continuum', continuum_data)
 
-    data = Model.GetScipyDistribution(parameters={'Signal_loc': 1.0, 'Background_loc': 0.0}).rvs(size=500)
-    print(data)
+    fit_model = signal + background + continuum
+    fit_model.fix_parameter('signal_scale')
+    fit_model.fix_parameter('background_scale')
+    fit_model.add_constraint('continuum_norm', lambda p: 1.0 - p['signal_norm'] - p['background_norm'])
 
-    r = scipy.optimize.minimize(Model.GetLoss('MaximumLikelihood'), Model.GetInitialParameters(), args=(data,), method='nelder-mead')
-    print(r)
+    data = fit_model.scipy.rvs(size=1000, signal_loc=1.0, background_loc=0.0, signal_norm=0.3, background_norm=0.5, continuum_norm=0.2, signal_scale=0.1, background_scale=1.0)
+    plt.hist(data, bins=100, range=(-3, 3), normed=True)
+    plt.show()
+
+    r = scipy.optimize.minimize(fit_model.get_maximumLikelihood_loss(), fit_model.get_initial_parameters(), args=(data,), method='nelder-mead')
+    print("Raw scipy result", r)
+    result = fit_model.get_result_parameters(r)
+    print("Result parameters", result)
     
     plt.hist(data, bins=100, range=(-3, 3), normed=True)
     X = np.linspace(-3, 3, 100)
-    plt.plot(X, Model(X, **Model.GetFreeParametersFromFitResult(r)))
+    plt.plot(X, fit_model.scipy.pdf(X, **result), label='Full fit_model')
+    for component in fit_model:
+        r = {key: value for key, value in result.items() if key.startswith(component.labels[0])}
+        plt.plot(X, component.scipy.pdf(X, **r), label=component.labels[0])
     plt.show()
-
-    print(Model.GetFreeParametersFromFitResult(r))
-
-
 
