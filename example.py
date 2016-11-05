79e63bc5f05dab81ad38b42dd094d8d0272d6a7d
Thomas Keck <thomas@thinkpad>
2016-11-06 00:01:00 +0100

    Likelihood Profile and Stability tests is working

--- example.py
+++ example.py
@@ -10,9 +10,13 @@ import scipy_extra.optimize
 import scipy_extra.fit
 
 
-def plot_data_and_model(data, fit_model):
-    plt.hist(data, bins=100, range=(-3, 3), normed=True)
-    plt.plot(X, fit_model.frozen_distribution.pdf(X), label=fit_model.name)
+def plot_data_and_model(title, data, fit_model, extended=False):
+    plt.title(title)
+    if extended:
+        plt.hist(data, bins=100, range=(-3, 3), weights=np.ones(len(data)) * 100.0/6.0)
+    else:
+        plt.hist(data, bins=100, range=(-3, 3), normed=True)
+    plt.plot(X, fit_model.norm * fit_model.frozen_distribution.pdf(X), label=fit_model.name)
     for name, norm, distribution in fit_model.get_frozen_components():
         plt.plot(X, norm * distribution.pdf(X), label=name)
     plt.legend()
@@ -31,20 +35,40 @@ if __name__ == '__main__':
     
     fit_model.fix_parameter('signal_scale')
     fit_model.fix_parameter('background_scale')
-    fit_model.add_constraint('continuum_norm', lambda p: 1.0 - p['signal_norm'] - p['background_norm'])
+    fit_model.add_constraint('continuum_norm', lambda p: max(1.0 - p['signal_norm'] - p['background_norm'], 0.0))
 
     data = fit_model.frozen_distribution.rvs(size=10000)
-    plot_data_and_model(data, fit_model)
+    plot_data_and_model("Model with true parameters", data, fit_model)
     print(fit_model.parameters)
 
     fit_model.set_parameters(signal_loc=0.5, background_loc=0.5, signal_norm=0.4, background_norm=0.3, continuum_norm=0.3)
-    plot_data_and_model(data, fit_model)
+    #fit_model.set_parameters(signal_loc=0.5, background_loc=0.5, signal_norm=4000, background_norm=3000, continuum_norm=3000)
+    plot_data_and_model("Model Before Fit", data, fit_model)
 
-    fitter = scipy_extra.fit.Fitter()
+    #fitter = scipy_extra.fit.Fitter(loss='extended-unbinned-maximum-likelihood')
+    fitter = scipy_extra.fit.Fitter(loss='unbinned-maximum-likelihood')
     result, r = fitter.fit(fit_model, data)
     print("Raw scipy result", r)
     print("Result parameters", result)
     fit_model.set_parameters(**result)
     
-    plot_data_and_model(data, fit_model)
+    plot_data_and_model("Model After Fit", data, fit_model)
+  
+    signal_yield_generated = np.linspace(0.01, 0.5, 20)
+    likelihood = fitter.likelihood_profile(fit_model, {'signal_norm': signal_yield_generated}, data)
+    plt.title("Likelihood Profile")
+    plt.xlabel("Generated Signal Yield")
+    plt.ylabel("- Log Likelihood")
+    plt.plot(signal_yield_generated, likelihood)
+    plt.show()
+
+    signal_yield_fitted = fitter.stability_test(fit_model, {'signal_norm': signal_yield_generated})['signal_norm']
+    plt.title("Stability test")
+    plt.xlabel("Generated Signal Yield")
+    plt.ylabel("Fitted Signal Yield")
+    plt.plot(signal_yield_generated, signal_yield_fitted)
+    plt.plot([0.0, 1.0], [0.0, 1.0])
+    plt.show()
+    print(signal_yield_generated)
+    print(signal_yield_fitted)
 
--- scipy_extra/fit.py
+++ scipy_extra/fit.py
@@ -8,6 +8,8 @@ import scipy.stats
 from . import stats
 
 from multiprocessing import Pool
+from itertools import cycle
+
 
 class Model(object):
     def __init__(self, name):
@@ -15,7 +17,7 @@ class Model(object):
         self.names = []
         self.distributions = []
         self.parameters = {}
-        self.constraints = {}
+        self.constraints = []
       
     @property
     def distribution(self):
@@ -24,6 +26,10 @@ class Model(object):
     @property
     def frozen_distribution(self):
         return stats.linear_model_gen(dict(zip(self.names, self.distributions)), name=self.name)(**self.parameters)
+    
+    @property
+    def norm(self):
+        return np.sum([self.parameters['{}_norm'.format(name)] for name in self.names])
 
     def __iter__(self):
         return iter(dict(zip(self.names, self.distributions)))
@@ -52,71 +58,102 @@ class Model(object):
         for parameter in parameters:
             if parameter in self.parameters:
                 self.parameters[parameter] = parameters[parameter]
+            else:
+                print("WARNING: Setting an unkown parameter {}".format(parameter))
+        for parameter, function in self.constraints:
+            self.parameters[parameter] = function(self.parameters)
 
     def add_constraint(self, parameter, function):
         if parameter not in self.parameters:
             print("WARNING: Adding constraint for {} but this parameter is not in the model".format(parameter))
-        self.constraints[parameter] = function
+        self.constraints = [(key, value) for key, value in self.constraints if key != parameter]
+        self.constraints.append((parameter, function))
         self.set_parameters(**{parameter: function(self.parameters)})
 
-    def fix_parameter(self, parameter):
-        self.add_constraint(parameter, lambda p: self.parameters[parameter])
+    def fix_parameter(self, parameter, priority=10):
+        self.constraints = [(key, value) for key, value in self.constraints if key != parameter]
+        self.constraints = [(parameter, lambda p: self.parameters[parameter])] + self.constraints
+        self.set_parameters(**{parameter: self.parameters[parameter]})
 
     def get_free_parameters(self):
-        return {parameter: value for parameter, value in self.parameters.items() if parameter not in self.constraints}
+        fixed_parameters = [c[0] for c in self.constraints]
+        return {parameter: value for parameter, value in self.parameters.items() if parameter not in fixed_parameters}
     
     def get_fixed_parameters(self):
-        return {parameter: value for parameter, value in self.parameters.items() if parameter in self.constraints}
+        fixed_parameters = [c[0] for c in self.constraints]
+        return {parameter: value for parameter, value in self.parameters.items() if parameter in fixed_parameters}
+
+
+def _likelihood_profile_task(args):
+    self, parameters, values, fit_model, data = args
+    fit_model.set_parameters(**{parameter: value for parameter, value in zip(parameters, values)})
+    _, r = self.fit(fit_model, data)
+    return r.fun
+
+
+def _stability_test_task(args):
+    # TODO Setting the parameters correctly for the fit is maybe to easy?
+    self, parameters, values, fit_model, data_size = args
+    fit_model.set_parameters(**{parameter: value for parameter, value in zip(parameters, values)})
+    data = fit_model.frozen_distribution.rvs(size=data_size)
+    fit_result, _ = self.fit(fit_model, data)
+    return [fit_result[parameter] for parameter in parameters]
 
 
 class Fitter(object):
-    def __init__(self, loss='maximum-likelihood', method='nelder-mead'):
-        if loss == 'maximum-likelihood':
-            self.loss = self._maximum_likelihood_loss
+    def __init__(self, loss='unbinned-maximum-likelihood', method='nelder-mead'):
+        if loss == 'unbinned-maximum-likelihood':
+            self.loss = self._unbinned_maximum_likelihood_loss
+        elif loss == 'extended-unbinned-maximum-likelihood':
+            self.loss = self._extended_unbinned_maximum_likelihood_loss
         else:
             raise RuntimeError("Unkown loss function named " + loss)
         self.method = method
 
-    def _maximum_likelihood_loss(self, free_parameters, data, fit_model):
+    def _get_current_parameters(self, free_parameters, fit_model):
         parameters = {key: free_parameters[i] for i, key in enumerate(fit_model.get_free_parameters().keys())}
-        for parameter, function in fit_model.constraints.items():
+        for parameter, function in fit_model.constraints:
             if parameter in fit_model.parameters:
                 parameters[parameter] = function(parameters)
-        return -np.sum(np.log(fit_model.distribution.pdf(data, **parameters)))
+        return parameters
 
+    def _unbinned_maximum_likelihood_loss(self, free_parameters, data, fit_model):
+        parameters = self._get_current_parameters(free_parameters, fit_model)
+        return -np.sum(np.log(fit_model.distribution.pdf(data, **parameters)))
+    
+    def _extended_unbinned_maximum_likelihood_loss(self, free_parameters, data, fit_model):
+        parameters = self._get_current_parameters(free_parameters, fit_model)
+        average_number_of_events = np.sum([parameters['{}_norm'.format(name)] for name in fit_model.names])
+        return self._unbinned_maximum_likelihood_loss(free_parameters, data, fit_model) + average_number_of_events
 
     def fit(self, fit_model, data):
         initial_parameters = np.array(list(fit_model.get_free_parameters().values()))
         r = scipy.optimize.minimize(self.loss, initial_parameters, args=(data, fit_model), method=self.method)
         parameters = dict(zip(fit_model.get_free_parameters().keys(), r.x))
-        for parameter, function in fit_model.constraints.items():
+        for parameter, function in fit_model.constraints:
             parameters[parameter] = function(parameters)
         return parameters, r
 
-    def liklihood_profile(self, fit_model, data, parameter, linspace):
-        parameters, _ = self.fit(fit_model, data)
-        fit_model.set_parameters(parameters)
-        fit_model.fix_parameter(parameter)
+    def likelihood_profile(self, fit_model, parameter_spaces, data):
+        parameters = parameter_spaces.keys()
+        linspaces = parameter_spaces.values()
 
-        def task(value):
-            fit_model.set_parameters(parameter=value)
-            _, r = self.fit(fit_model, data)
-            return r
+        best_fit_parameters, _ = self.fit(fit_model, data)
+        fit_model.set_parameters(**best_fit_parameters)
 
-        with Pool(processes=4) as pool:
-            results = pool.map(task, linspace)
-        print(results)
+        for parameter in parameters:
+            fit_model.fix_parameter(parameter)
+
+        #with Pool(processes=4) as pool:
+        arguments = list(zip(cycle([self]), cycle([parameters]), zip(*linspaces), cycle([fit_model]), cycle([data])))
+        results = list(map(_likelihood_profile_task, arguments))
         return results
 
-    def stability_test(self, fit_model, parameter, linspace):
-        def task(value):
-            fit_model.set_parameters(parameter=value)
-            data = fit_model.frozen_distribution.rvs(size=10000)
-            parameters, _ = self.fit(fit_model, data)
-            return parameters
-        
-        with Pool(processes=4) as pool:
-            results = pool.map(task, linspace)
-        print(results)
-        return results
+    def stability_test(self, fit_model, parameter_spaces, data_size=10000):
+        #with Pool(processes=4) as pool:
+        parameters = parameter_spaces.keys()
+        linspaces = parameter_spaces.values()
+        arguments = list(zip(cycle([self]), cycle([parameters]), zip(*linspaces), cycle([fit_model]), cycle([data_size])))
+        results = list(map(_stability_test_task, arguments))
+        return {parameter: values for parameter, *values in zip(parameters, *results)}
 
