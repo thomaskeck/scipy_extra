1dd541eeb8bb20809654525f5d838ca3ee201a2e
Thomas Keck <thomas.keck2@kit.edu>
2018-01-23 14:07:50 +0100

    Added information to the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 
 setup(name='scipy_extra',
-      version='',
-      package_dir={ '': '.' },
+      author='Thomas Keck',
+      author_email='t.keck@online.de',
+      url='https://github.com/thomaskeck/scipy_extra',
+      keywords='scipy, maximum likelihood, fitting, HEP',
+      install_requires=['scipy', 'numpy', 'matplotlib'],
+      python_requires='>=3',
+      version='0.1',
       packages=['scipy_extra'])
 
